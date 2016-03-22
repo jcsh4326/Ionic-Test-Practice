bea349e2b41d4d0bfe13fa77f69dce92b0d5e689
瑾诚 <jincheng.jcs@gmail.com>
2016-03-23 07:01:56 +0800

    TODO: Spy Scope

--- www/karma.conf.js
+++ www/karma.conf.js
@@ -14,9 +14,10 @@ module.exports = function(config) {
 
 
     // list of files / patterns to load in the browser
-    files: [
-        'lib/ionic/js/ionic.bundle.js',
+    files: [        
+        'lib/ionic/js/ionic.bundle.js',        
         'js/**/*.js',
+        '../bower_components/angular-mocks/angular-mocks.js',
         'test/**/*.test.js'
     ],
 
@@ -48,7 +49,8 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
+    // logLevel: config.LOG_INFO,
 
 
     // enable / disable watching file and executing tests whenever any file changes
--- www/test/chatsCtrl.test.js
+++ www/test/chatsCtrl.test.js
@@ -1,13 +1,49 @@
 describe('ChatsCtrl', function () {
-    var chatsMock;
+    var chatsMock, chats, ctrl, scope;
+    
+    // load module for app
+    beforeEach(module('starter'));
+
+    beforeEach(inject(function($controller,$rootScope){        
+        chats = [{
+            id: 0,
+            name: 'Ben Sparrow',
+            lastText: 'You on your way?',
+            face: 'img/ben.png'
+        }];        
+        var remove = function (chat) {
+            chats.splice(chats.indexOf(chat), 1);
+        }
+        var all = function () {
+            return chats;
+        }
+        chatsMock = {
+            all: all,             
+            remove: remove
+        }        
+        
+        
+        // chatsMock = jasmine.createSpyObj('searchService', ['all', 'remove']);
+        
+        scope = $rootScope.$new();                
+        ctrl = $controller('ChatsCtrl',{
+            '$scope': scope,
+            'Chats': chatsMock
+        });                
+    }));
+
     describe('#doCallAllChats', function () {
         it('should call all the chats info', function () {
-            expect(chatsMock.all);
+            if(chatsMock.all){
+                spyOn(chatsMock, 'all');
+                spyOn(chatsMock, 'remove');
+                expect(scope.chats).toHaveBeenCalled();    
+            }                          
         });
     });
     describe('#doRemoveOneChat', function () {
         it('should remove the special one', function () {
-            expect(chatsMock.remove).toHaveBeenCalledWith(chat);
+            // expect(chatsMock.remove).toHaveBeenCalledWith(chat);
         });
     });
 })
